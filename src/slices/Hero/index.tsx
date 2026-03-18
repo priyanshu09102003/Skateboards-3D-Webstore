39fc507addcf1b2b09b111087df642dd5b8b25f7
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-03-18 19:14:02 +0530

    Files and texturing

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -3,6 +3,9 @@ import { Content } from "@prismicio/client";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
 
+
+import { Bounded } from "@/components/Bounded";
+
 /**
  * Props for `Hero`.
  */
@@ -13,14 +16,15 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero: FC<HeroProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-brand-pink relative relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
       <PrismicRichText field={slice.primary.heading} />
       <PrismicRichText field={slice.primary.body} />
       <PrismicNextLink field={slice.primary.button} />
-    </section>
+    </Bounded>
   );
 };
 
