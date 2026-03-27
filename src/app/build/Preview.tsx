395b9a52c3012317458a46d056f1502d9aff9be2
Priyanshu Paul <priyanshupaul90@gmail.com>
2026-03-28 02:57:18 +0530

    Changing camera angle on selecting customization for each component

--- src/app/build/Preview.tsx
+++ src/app/build/Preview.tsx
@@ -2,7 +2,7 @@
 
 import { CameraControls, Environment, Preload, useTexture } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useEffect, useRef } from 'react'
 import { useCustomizerControls } from './context';
 import { asImageSrc } from '@prismicio/client';
 import { Skateboard } from '@/components/Skateboard';
@@ -31,6 +31,41 @@ export default function Preview({wheelTextureURLs, deckTextureURLs}: Props) {
     const deckTextureURL = asImageSrc(selectedDeck?.texture)?? DEFAULT_DECK_TEXTURE
     const truckColor = selectedTruck?.color ?? DEFAULT_TRUCK_COLOR
     const boltColor = selectedBolt?.color ?? DEFAULT_BOLT_COLOR
+
+    function setCameraControls(target: THREE.Vector3, pos:THREE.Vector3){
+        if(!cameraControls.current) return;
+
+        cameraControls.current.setTarget(target.x, target.y, target.z, true)
+        cameraControls.current.setPosition(pos.x, pos.y, pos.z, true)
+    }
+
+        useEffect(() => {
+            setCameraControls(
+            new THREE.Vector3(0, 0.3, 0),
+            new THREE.Vector3(1.5, 0.8, 0)
+            );
+        }, [selectedDeck]);
+
+        useEffect(() => {
+            setCameraControls(
+            new THREE.Vector3(-0.12, 0.29, 0.57),
+            new THREE.Vector3(0.1, 0.25, 0.9)
+            );
+        }, [selectedTruck]);
+
+        useEffect(() => {
+            setCameraControls(
+            new THREE.Vector3(-0.08, 0.54, 0.64),
+            new THREE.Vector3(0.09, 1, 0.9)
+            );
+        }, [selectedWheel]);
+
+        useEffect(() => {
+            setCameraControls(
+            new THREE.Vector3(-0.25, 0.3, 0.62),
+            new THREE.Vector3(-0.5, 0.35, 0.8)
+            );
+        }, [selectedBolt]);
 
     function onCameraControlStart(){
         if(!cameraControls.current || !floorRef.current || cameraControls.current.colliderMeshes.length > 0) return
